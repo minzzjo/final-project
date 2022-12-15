601a940388dc33638abb1ee94f83fd019926e768
minzzjo <minxxjozz@gmail.com>
2022-12-15 12:44:43 +0900

    resizing

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import store from "./redux/config/configStore";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
-import GlobalFont from './fonts/globalFont';
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// yarn add react-is
 root.render(
-    <>
-    <BrowserRouter />
-    <Provider store={store}>
-      <GlobalFont/>
-        <App />
-    </Provider>
-    </>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
